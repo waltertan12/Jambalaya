472e1b27b794b0213e759df77eeae8a776999529
Walter Tan <wtan@coursehero.com>
2017-05-19 15:14:25 +0200

    Set up local environment to use Flow

---
+++ dist/src/server.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
